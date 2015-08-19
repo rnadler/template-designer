c8a5c1bb35c5df978d93e0b49f919cfa09873468
rnadler <robert.nadler@gmail.com>
2015-08-18 19:50:15 -0700

    Change default temaples to be US and FR Primary

--- spec/home_spec.js
+++ spec/home_spec.js
@@ -12,7 +12,7 @@ describe('template designer homepage', function() {
 
   it('should have tabs with correct headings', function() {
     expect(element(by.id('project')).getText()).toEqual('Project []');
-    expect(element(by.id('groups')).getText()).toEqual('Rule Groups [12]');
+    expect(element(by.id('groups')).getText()).toEqual('Rule Groups [20]');
     expect(element(by.id('rules')).getText()).toEqual('Compliance Rules [3]');
     expect(element(by.id('templates')).getText()).toEqual('Templates [2]');
     expect(element(by.id('language')).getText()).toEqual('English US [en_US]');
@@ -21,6 +21,6 @@ describe('template designer homepage', function() {
 
   it('should have language selections', function() {
     expect(element(by.id('language')).getText()).toEqual('English US [en_US]');
-    expect(element.all(by.repeater('l in languages')).count()).toEqual(7);
+    expect(element.all(by.repeater('l in languages')).count()).toEqual(8);
   });
 });
